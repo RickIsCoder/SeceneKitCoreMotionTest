3b56e65ae523e7fd8545fb2f0e90d6c1e9e31a08
Rick li <285699093@qq.com>
2015-12-02 19:11:20 +0800

    12-2

--- SeceneCoreMotionTest/SeceneCoreMotionTest/GameViewController.swift
+++ SeceneCoreMotionTest/SeceneCoreMotionTest/GameViewController.swift
@@ -127,58 +127,10 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate {
         diffuseLightNode.position = SCNVector3(x: 0, y: 0, z: 0)
         scene.rootNode.addChildNode(diffuseLightNode)
         
-        let material = SCNMaterial()
-        material.diffuse.contents = UIColor.redColor()
-        material.locksAmbientWithDiffuse = true
-        let material2 = SCNMaterial()
-        material2.diffuse.contents = UIColor.whiteColor()
-        material2.locksAmbientWithDiffuse = true
-        let material3 = SCNMaterial()
-        material3.diffuse.contents = UIColor.blueColor()
-        material3.locksAmbientWithDiffuse = true
-        let material4 = SCNMaterial()
-        material4.diffuse.contents = UIColor.purpleColor()
-        material4.locksAmbientWithDiffuse = true
-        let material5 = SCNMaterial()
-        material5.diffuse.contents = UIColor.yellowColor()
-        material5.locksAmbientWithDiffuse = true
-        let material6 = SCNMaterial()
-        material6.diffuse.contents = UIColor.orangeColor()
-        material6.locksAmbientWithDiffuse = true
-        
-        
-        //Create the box
-        let baseBox = SCNBox(width: 10, height: 10, length: 10, chamferRadius: 0)
-        
-        baseBox.materials = [material2, material2, material3, material4, material5, material6]
-        let boxNode = SCNNode(geometry: baseBox)
-        boxNode.position = SCNVector3(x: 0, y: -10, z: 0)
-        
-        // Create planes
-//        let planesFront = SCNPlane(width: 20, height: 20)
-//        planesFront.firstMaterial!.doubleSided = true
-//        planesFront.firstMaterial!.diffuse.contents = UIColor.redColor()
-//        let planesFrontNode = SCNNode(geometry: planesFront)
-//        planesFrontNode.position = SCNVector3(x: 0, y: 0, z: -20)
-        
 //        let textFront = SCNText(string: "这是前面", extrusionDepth: 0.0)
 //        textFront.font = UIFont(name: "Arial", size: 2)
 //        let textFrontNode = SCNNode(geometry: textFront)
 //        textFrontNode.position = SCNVector3(0, 0, -15)
-//        
-//        let tapText = SCNText(string: "点我翻页", extrusionDepth: 0.0)
-//        tapText.font = UIFont(name: "Arial", size: 2)
-//        self.tapTextNode = SCNNode(geometry: tapText)
-//        self.tapTextNode!.position = SCNVector3(15, 3, 0)
-//        self.tapTextNode!.eulerAngles = SCNVector3(0, GLKMathDegreesToRadians(-90), 0)
-        
-//        let planesFrontLeft = SCNPlane(width: 20, height: 20)
-//        planesFrontLeft.firstMaterial!.doubleSided = true
-//        planesFrontLeft.firstMaterial!.diffuse.contents = UIColor.greenColor()
-//        let planesFrontLeftNode = SCNNode(geometry: planesFrontLeft)
-//        planesFrontLeftNode.pivot = SCNMatrix4MakeRotation(Float(M_PI_4)*3, 0, 1, 0)
-//        planesFrontLeftNode.position = SCNVector3(-15, 0, 0)
-        
         
 //        let planesTop = SCNPlane(width: 20, height: 20)
 //        planesTop.firstMaterial!.doubleSided = true
@@ -188,78 +140,16 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate {
 //        planesTopNode.eulerAngles = SCNVector3(GLKMathDegreesToRadians(90), 0, 0)
 //        planesTopNode.position = SCNVector3(x: 0, y: 10, z: 0)
         
-//        let planesBack = SCNPlane(width: 20, height: 20)
-//        planesBack.firstMaterial!.doubleSided = true
-//        planesBack.firstMaterial!.diffuse.contents = UIColor.whiteColor()
-//        let planesBackNode = SCNNode(geometry: planesBack)
-//        planesBackNode.eulerAngles = SCNVector3(0, GLKMathDegreesToRadians(180), 0)
-//        planesBackNode.position = SCNVector3(x: 0, y: 0, z: 20)
-        
-//        let planesBottom = SCNPlane(width: 20, height: 20)
-//        planesBottom.firstMaterial!.doubleSided = true
-//        planesBottom.firstMaterial!.diffuse.contents = UIColor.greenColor()
-//        let planesBottomNode = SCNNode(geometry: planesBottom)
-//        //planesBottomNode.pivot = SCNMatrix4MakeRotation(Float(M_PI_2), 1, 0, 0)
-//        planesBottomNode.eulerAngles = SCNVector3(GLKMathDegreesToRadians(-90), 0, 0)
-//        planesBottomNode.position = SCNVector3(x: 0, y: -10, z: 0)
-        
-//        let planesLeft = SCNPlane(width: 20, height: 20)
-//        planesLeft.firstMaterial!.doubleSided = true
-//        planesLeft.firstMaterial!.diffuse.contents = UIColor.purpleColor()
-//        let planesLeftNode = SCNNode(geometry: planesLeft)
-//        planesLeftNode.pivot = SCNMatrix4MakeRotation(Float(M_PI_2), 0, 1, 0)
-//        planesLeftNode.position = SCNVector3(x: -20, y: 0, z: 0)
-        
-//        let planesRight = SCNPlane(width: 20, height: 20)
-//        planesRight.firstMaterial!.doubleSided = true
-//        planesRight.firstMaterial!.diffuse.contents = UIColor.orangeColor()
-//        let planesRightNode = SCNNode(geometry: planesRight)
-//        planesRightNode.pivot = SCNMatrix4MakeRotation(Float(M_PI_2), 0, 1, 0)
-//        planesRightNode.position = SCNVector3(x: 20, y: 0, z: 0)
-        
-        let planesFrontRight = SCNPlane(width: 10, height: 15)
-        planesFrontRight.firstMaterial!.doubleSided = true
-        planesFrontRight.firstMaterial!.diffuse.contents = UIImage(named: "UFO")
-        let planesFrontRightNode = SCNNode(geometry: planesFrontRight)
-        planesFrontRightNode.pivot = SCNMatrix4MakeRotation(Float(M_PI_4), 0, 1, 0)
-        planesFrontRightNode.position = SCNVector3(10, 0, -15)
-        
-        
-//        let tapBox = SCNBox(width: 5, height: 5, length: 5, chamferRadius: 1)
-//        tapBox.firstMaterial!.diffuse.contents = UIColor.redColor()
-//        self.boxNode = SCNNode(geometry: tapBox)
-//        self.boxNode!.position = SCNVector3(0, 4, -10)
-        
-        
-//        let tube = SCNTube(innerRadius: 4, outerRadius: 5, height: 10)
-//        tube.firstMaterial!.diffuse.contents = UIImage(named: "ground")
-//        let tubeNode = SCNNode(geometry: tube)
-//        tubeNode.position = SCNVector3(-10, 5, -15)
-        
-        
-        let guitar = SCNPlane(width: 10, height: 10)
-        guitar.firstMaterial!.diffuse.contents = UIImage(named: "guitar")
-        let guitarNode = SCNNode(geometry: guitar)
-        guitarNode.position = SCNVector3(-5, 5, -15)
-        //guitarNode.eulerAngles = SCNVector3(0, GLKMathDegreesToRadians(80), 0)
-        scene.rootNode.addChildNode(guitarNode)
-        
-        let boon = SCNPlane(width: 10, height: 10)
-        boon.firstMaterial!.diffuse.contents = UIImage(named: "boon")
-        let boonNode = SCNNode(geometry: boon)
-        boonNode.position = SCNVector3(-10, 5, 0)
-        boonNode.eulerAngles = SCNVector3(0, GLKMathDegreesToRadians(90), 0)
-        scene.rootNode.addChildNode(boonNode)
-        
-        
         createItemNode(5, height: 10, imageName: "bgItem1", position: SCNVector3(10, 5, -10), angle: SCNVector3(0, GLKMathDegreesToRadians(-30), 0), scene: scene)
         createItemNode(10, height: 10, imageName: "bgItem2", position: SCNVector3(15, 10, 0), angle: SCNVector3(0, GLKMathDegreesToRadians(-90), 0), scene: scene)
         createItemNode(10, height: 10, imageName: "bgItem3", position: SCNVector3(15, 0, 10), angle: SCNVector3(0, GLKMathDegreesToRadians(-120), 0), scene: scene)
         createItemNode(10, height: 10, imageName: "bgItem4", position: SCNVector3(-15, 0, 0), angle: SCNVector3(0, GLKMathDegreesToRadians(90), 0), scene: scene)
         createItemNode(10, height: 10, imageName: "bgItem5", position: SCNVector3(-15, 0, 10), angle: SCNVector3(0, GLKMathDegreesToRadians(120), 0), scene: scene)
         createItemNode(10, height: 10, imageName: "bgItem6", position: SCNVector3(0, 0, 15), angle: SCNVector3(0, GLKMathDegreesToRadians(180), 0), scene: scene)
-        //createItemNode(15, height: 5, imageName: "bgItem7", position: SCNVector3(0, 10, 15), angle: SCNVector3(0, GLKMathDegreesToRadians(145), 0), scene: scene)
-        //createItemNode(10, height: 5, imageName: "meteor", position: SCNVector3(0, 10, -10), angle: SCNVector3(0, 0, 0), scene: scene)
+        createItemNode(10, height: 15, imageName: "UFO", position: SCNVector3(10, 0, -15), angle: SCNVector3(0, GLKMathDegreesToRadians(-45), 0), scene: scene)
+        createItemNode(10, height: 10, imageName: "guitar", position: SCNVector3(-5, 5, -15), angle: nil, scene: scene)
+        createItemNode(10, height: 10, imageName: "boon", position: SCNVector3(-10, 5, 0), angle: SCNVector3(0, GLKMathDegreesToRadians(90), 0), scene: scene)
+        
         
         let meteor = SCNPlane(width: 10, height: 5)
         meteor.firstMaterial!.diffuse.contents = UIImage(named: "meteor")
@@ -267,12 +157,31 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate {
         meteorNode.position = SCNVector3(0, 10, -10)
         scene.rootNode.addChildNode(meteorNode)
         
+        
+        // rotation fire meteor
+        // the meteor parent node
+        let meteorParentNode = SCNNode()
+        meteorParentNode.position = SCNVector3(0, 0, 0)
+        scene.rootNode.addChildNode(meteorParentNode)
+        
+        // the meteor rotation node (the center of rotation of the earth around the sun)
+        let earthRotationNode = SCNNode()
+        meteorParentNode.addChildNode(earthRotationNode)
+        
+        // the meteor node
         let fireMeteor = SCNPlane(width: 10, height: 2)
         fireMeteor.firstMaterial!.diffuse.contents = UIImage(named: "bgItem7")
         let fireMeteorNode = SCNNode(geometry: fireMeteor)
-        fireMeteorNode.position = SCNVector3(-5, 9, -15)
+        fireMeteorNode.position = SCNVector3(-5, 1, -20)
         //fireMeteorNode.eulerAngles = SCNVector3(0, GLKMathDegreesToRadians(145), 0)
-        scene.rootNode.addChildNode(fireMeteorNode)
+        earthRotationNode.addChildNode(fireMeteorNode)
+        
+        // rotate the Earth and the Sun
+        let animation: CABasicAnimation = CABasicAnimation(keyPath: "rotation")
+        animation.duration = 10.0
+        animation.toValue = NSValue(SCNVector4: SCNVector4(0, -1, 0, M_PI*2))
+        animation.repeatCount = FLT_MAX
+        earthRotationNode.addAnimation(animation, forKey: "earth rotation around sun")
         
         
         // sky 
@@ -286,27 +195,6 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate {
         let floorNode = makeFloor()
         floorNode.position = SCNVector3(x: 0, y: -20, z: 0)
         scene.rootNode.addChildNode(floorNode)
-        
-        
-       // scene.rootNode.addChildNode(self.boxNode!)
-        
-        //scene.rootNode.addChildNode(planesFrontNode)
-        //scene.rootNode.addChildNode(planesTopNode)
-        //scene.rootNode.addChildNode(planesBackNode)
-        //scene.rootNode.addChildNode(planesBottomNode)
-        //scene.rootNode.addChildNode(planesLeftNode)
-        //scene.rootNode.addChildNode(planesRightNode)
-        //scene.rootNode.addChildNode(tapTextNode)
-        
-        //scene.rootNode.addChildNode(textFrontNode)
-        
-        //scene.rootNode.addChildNode(planesFrontLeftNode)
-        scene.rootNode.addChildNode(planesFrontRightNode)
-        
-//        scene.rootNode.addChildNode(tubeNode)
-        
-        //scene.rootNode.childNodeWithName("ship", recursively: true)
-        
         
         
         // add node from another scene
@@ -342,7 +230,6 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate {
         self.scnView.addGestureRecognizer(tapGesture)
         
         meteorAction(meteorNode)
-        fireMeteorAction(fireMeteorNode)
     }
     
     func degreesToRadians(degrees: Float) -> Float {
@@ -359,29 +246,21 @@ class GameViewController: UIViewController, SCNSceneRendererDelegate {
         meteor.runAction(action)
     }
     
-    func fireMeteorAction(meteor: SCNNode) {
-//        let frontToRight = SCNAction.moveTo(SCNVector3(15, 9, 0), duration: 5)
-//        let rightToBehind = SCNAction.moveTo(SCNVector3(0, 9, 15), duration: 5)
-//        let behindToLeft = SCNAction.moveTo(SCNVector3(-15, 9, 0), duration: 5)
-//        let leftToFront = SCNAction.moveTo(SCNVector3(0, 9, -15), duration: 5)
-        let animation = CAKeyframeAnimation(keyPath: "position")
-        animation.values = [
-            NSValue(SCNVector3: SCNVector3(0, 0, -15)),
-            NSValue(SCNVector3: SCNVector3(15, 0, 0)),
-            NSValue(SCNVector3: SCNVector3(0, 0, 15)),
-            NSValue(SCNVector3: SCNVector3(-15, 0, 0)),
-        ]
-        animation.duration = 15
-        meteor.addAnimation(animation, forKey: "orbit")
-    }
-    
-    //let planesBottom = SCNPlane(width: 20, height: 20)
-    //        planesBottom.firstMaterial!.doubleSided = true
-    //        planesBottom.firstMaterial!.diffuse.contents = UIColor.greenColor()
-    //        let planesBottomNode = SCNNode(geometry: planesBottom)
-    //        //planesBottomNode.pivot = SCNMatrix4MakeRotation(Float(M_PI_2), 1, 0, 0)
-    //        planesBottomNode.eulerAngles = SCNVector3(GLKMathDegreesToRadians(-90), 0, 0)
-    //        planesBottomNode.position = SCNVector3(x: 0, y: -10, z: 0)
+//    func fireMeteorAction(meteor: SCNNode) {
+////        let frontToRight = SCNAction.moveTo(SCNVector3(15, 9, 0), duration: 5)
+////        let rightToBehind = SCNAction.moveTo(SCNVector3(0, 9, 15), duration: 5)
+////        let behindToLeft = SCNAction.moveTo(SCNVector3(-15, 9, 0), duration: 5)
+////        let leftToFront = SCNAction.moveTo(SCNVector3(0, 9, -15), duration: 5)
+//        let animation = CAKeyframeAnimation(keyPath: "position")
+//        animation.values = [
+//            NSValue(SCNVector3: SCNVector3(0, 0, -15)),
+//            NSValue(SCNVector3: SCNVector3(15, 0, 0)),
+//            NSValue(SCNVector3: SCNVector3(0, 0, 15)),
+//            NSValue(SCNVector3: SCNVector3(-15, 0, 0)),
+//        ]
+//        animation.duration = 15
+//        meteor.addAnimation(animation, forKey: "orbit")
+//    }
     
     func createItemNode(width: CGFloat, height: CGFloat, imageName: String, position: SCNVector3, angle: SCNVector3?, scene: SCNScene) {
         let itemPlane = SCNPlane(width: width, height: height)
